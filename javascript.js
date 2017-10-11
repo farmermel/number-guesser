27d4ee7587263eacaec661dca04589d537a3ddfc
Melena Suliteanu <melenasuliteanu@gmail.com>
2017-10-11 12:53:05 -0600

    Additional work on adding Hitchhikers Guide easter egg. Not yet working

--- javascript.js
+++ javascript.js
@@ -9,7 +9,7 @@ console.log('Javascript is linked succesfully!')
 //   var idSelection = document.getElementById(id);
 //   return idSelection;
 // }
-
+//select elements from HTML
 var numberGuessed= document.querySelector('p.number-guessed');
 var selectButton = document.querySelector('#submit');
 var userInput = document.getElementById('enter-guess');
@@ -18,31 +18,24 @@ var enterMinField = document.querySelector('#set-minimum');
 var enterMaxField = document.querySelector('#set-maximum');
 var setRangeButton = document.querySelector('#range-button');
 var changePrintedRange = document.querySelector('.printed-range');
+var hiddenMarvinImg = document.querySelector('img');
 
+//other variables
 var minimum = 0;
 var maximum = 100;
 
+var randomNum = generateCorrectAnswer();
+
+//Event listeners
+//on click of submit button, run submit function to change display of number guessed. Also run areTheyRight function which determines whether they're correct and implements desired behavior. 
 
 selectButton.addEventListener('click', function() {
   submit(event);
-  areTheyRight()
+  areTheyRight();
 
 });
 
-var randomNum = generateCorrectAnswer();
-
-function generateCorrectAnswer() {
-  minimum = Math.ceil(minimum);
-  maximum = Math.floor(maximum);
-  randomNum = Math.floor(Math.random() * (maximum - minimum)) + minimum;
-  console.log(randomNum);
-  return randomNum
-};
-
-function changePrintedRangeText(minimum, maximum) {
-  changePrintedRange.innerHTML = "Enter a number between <span>" + minimum + "</span> and <span>" + maximum + "</span>";
-  console.log('Hi I change the printed range!')
-}
+//on click the set range button will assign entered value to minimum and maximum, and call generateCorrectAnswer and changePrintedRange to update the minimum and maximum in relevant places
 
 setRangeButton.addEventListener('click', function() {
   event.preventDefault();
@@ -52,22 +45,54 @@ setRangeButton.addEventListener('click', function() {
   changePrintedRangeText(minimum, maximum);
 });
 
+//generate the correct answer based on minimum and maximum
+
+function generateCorrectAnswer() {
+  minimum = Math.ceil(minimum);
+  maximum = Math.floor(maximum);
+  randomNum = Math.floor(Math.random() * (maximum - minimum)) + minimum;
+  console.log(randomNum);
+  return randomNum
+};
+
+//change the range that is printed at the top of the page
+function changePrintedRangeText(minimum, maximum) {
+  changePrintedRange.innerHTML = "Enter a number between <span>" + minimum + "</span> and <span>" + maximum + "</span>";
+  console.log('Hi I change the printed range!')
+}
+
 
 function submit(event) {
   event.preventDefault();
+  // if (numberGuessed === 42 && randomNum === 42) {
+  // unHideMarvin();
+  // console.log('unhiding marvin');
+  // } else {
   console.log('function submit is running');
   numberGuessed.innerText = userInput.value;
   console.log(minimum);
+  // }
 };
 
 var rangeArray = [minimum, maximum];
-//rangeArray seems like a promising approach. It's making the changes! Now need to make the adjust range function call, and also need to update texts etc. Make sure it's adjust minimum and maximum everywhere!!!
+
+//decide if user is right, if they are implement necessary behaviors
 
 function areTheyRight() {
   var userGuess = parseInt(userInput.value);
   console.log(userGuess);
   console.log(randomNum);
-  if (userGuess === randomNum) {
+  if (userGuess === randomNum && userGuess === 42) {
+    console.log('guess was Marvin');
+    gameFeedback.innerText = "Grab a towel!";
+    winAdjust(minimum, maximum);
+    minimum = rangeArray[0];
+    maximum = rangeArray[1];
+    generateCorrectAnswer();
+    console.log(randomNum);
+    changePrintedRangeText(minimum, maximum);
+    unHideMarvin();
+  } else if (userGuess === randomNum){
     console.log('guess was right');
     gameFeedback.innerText = "BOOM!";
     winAdjust(minimum, maximum);
@@ -75,7 +100,7 @@ function areTheyRight() {
     maximum = rangeArray[1];
     generateCorrectAnswer();
     console.log(randomNum);
-    changePrintedRangeText(minimum, maximum)
+    changePrintedRangeText(minimum, maximum);
   } else if (userGuess > maximum || userGuess < minimum) {
     gameFeedback.innerText = "Enter a number in range!";
   } else if (userGuess < randomNum) {
@@ -88,6 +113,8 @@ function areTheyRight() {
   }
 };
 
+//adjust range when user wins
+
 function winAdjust(minimum, maximum) {
   minimum -= 10;
   maximum += 10;
@@ -98,16 +125,6 @@ function winAdjust(minimum, maximum) {
 }
 
 var clearButton = document.querySelector('.clear-button');
-
-// userInput.addEventListener('keydown', function() {
-//   if (clearButton.getAttribute('disabled') === '') { 
-//     clearButton.setAttribute('disabled', 'true');
-//   } else {clearButton.removeAttribute('disabled');
-//   console.log('keydown working in disable button');
-//   }
-// });
-
-//working turn clear button back on
 
 //disable and reenable the clear button
 
@@ -121,7 +138,7 @@ clearButton.addEventListener('click', function() {
   clearButton.setAttribute('disabled', 'true');
   resetButton.setAttribute('disabled', 'true');
   userInput.value = '';
-  numberGuessed.innerText = "* ; *"
+  numberGuessed.innerText = "-"
 });
 
 //disable and reenable the reset button
@@ -136,8 +153,16 @@ resetButton.addEventListener('click', function() {
   resetButton.setAttribute('disabled', 'true');
   clearButton.setAttribute('disabled', 'true');
   userInput.value = '';
-  numberGuessed.innerText = "* ; *"
+  numberGuessed.innerText = "-"
   generateCorrectAnswer()
 });
 
+function unHideMarvin() {
+  hiddenMarvinImg.style.removeProperty('display');
+  // console.log(hiddenMarvinImg.hasAttribute('hidden'));
+  // hiddenMarvinImg.removeAttribute('hidden');
+  // console.log('tried to show Marvin');
+  // numberGuessed.innerText = hiddenMarvinImg;
+  // console.log(hiddenMarvinImg.hasAttribute('hidden'));
+};
 
