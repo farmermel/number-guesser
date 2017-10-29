9a6cbb6ef53b9fe8f65315ec1c63852f6f7b514b
Melena Suliteanu <melenasuliteanu@gmail.com>
2017-10-29 09:19:50 -0600

    Clean up spaces and console logs from code

--- javascript.js
+++ javascript.js
@@ -1,6 +1,3 @@
-console.log('Javascript is linked succesfully!')
-
-//select elements from HTML
 var numberGuessed= document.querySelector('p.number-guessed');
 var selectButton = document.querySelector('#submit');
 var userInput = document.getElementById('enter-guess');
@@ -12,7 +9,6 @@ var changePrintedRange = document.querySelector('.printed-range');
 var hiddenMarvinImg = document.querySelector('img');
 var clearButton = document.querySelector('.clear-button');
 
-//other variables
 var minimum = 0;
 var maximum = 100;
 
@@ -20,16 +16,11 @@ var randomNum;
 
 generateCorrectAnswer();
 
-//EVENT LISTENERS
-//on click of submit button, run submit function to change display of number guessed. Also run areTheyRight function which determines whether they're correct and implements desired behavior. 
-
 selectButton.addEventListener('click', function(event) {
   submit(event);
   areTheyRight();
 
 });
-
-//on click the set range button will assign entered value to minimum and maximum, and call generateCorrectAnswer and changePrintedRange to update the minimum and maximum in relevant places
 
 setRangeButton.addEventListener('click', function(event) {
   event.preventDefault();
@@ -39,8 +30,6 @@ setRangeButton.addEventListener('click', function(event) {
   changePrintedRangeText(minimum, maximum);
 });
 
-//generate the correct answer based on minimum and maximum
-
 function generateCorrectAnswer() {
   minimum = Math.ceil(minimum);
   maximum = Math.floor(maximum);
@@ -48,32 +37,23 @@ function generateCorrectAnswer() {
   console.log(randomNum);
 };
 
-//change the range that is printed at the top of the page
 function changePrintedRangeText(minimum, maximum) {
   changePrintedRange.innerHTML = "Enter a number between <span>" + minimum + "</span> and <span>" + maximum + "</span>";
-  console.log('Hi I change the printed range!')
-}
-
+};
 
 function submit(event) {
   event.preventDefault();
   if (numberGuessed === 42 && randomNum === 42) {
-  unHideMarvin();
-  console.log('unhiding marvin');
+    unHideMarvin();
   } else { 
-  numberGuessed.innerText = userInput.value;
-  }
+    numberGuessed.innerText = userInput.value;
+  };
 };
 
 var rangeArray = [minimum, maximum];
 
-//decide if user is right, if they are implement necessary behaviors
-
-
 function areTheyRight() {
   var userGuess = parseInt(userInput.value);
-  console.log(userGuess);
-  console.log(randomNum);
   if (userGuess === randomNum && userGuess === 42) {
     easterEggBehavior();
   } else if (userGuess === randomNum){
@@ -83,35 +63,24 @@ function areTheyRight() {
   } else if (userGuess < randomNum) {
     tooLow();
   } else if (userGuess > randomNum) {
-    console.log('too high if statement is working')
     tooHigh();
   } else { 
     notANumber();
   };
 };
 
-//input reaction functions
-
 function easterEggBehavior() {
-  console.log('guess was Marvin');
   gameFeedback.innerText = "Grab a towel!";
   winAdjust(minimum, maximum);
-  //minimum = rangeArray[0];
-  //maximum = rangeArray[1];
   generateCorrectAnswer();
-  console.log(randomNum);
   changePrintedRangeText(minimum, maximum);
   unHideMarvin();
 };
 
 function winBehavior() {
-  console.log('guess was right');
     gameFeedback.innerText = "BOOM!";
     winAdjust(minimum, maximum);
-    //minimum = rangeArray[0];
-    //maximum = rangeArray[1];
     generateCorrectAnswer();
-    console.log(randomNum);
     changePrintedRangeText(minimum, maximum);
 };
 
@@ -131,18 +100,10 @@ function notANumber() {
   gameFeedback.innerText = "Nice try, enter a real number.";
 };
 
-//adjust range when user wins
-
 function winAdjust(minimum, maximum) {
   minimum -= 10;
   maximum += 10;
-  //rangeArray = [minimum, maximum];
-  console.log('winAdjust is being called');
-  console.log(rangeArray[1]);
-  //return rangeArray;
-}
-
-//disable and reenable the clear button
+};
 
 userInput.addEventListener('keydown', function() {
   clearButton.removeAttribute('disabled');
@@ -155,8 +116,6 @@ clearButton.addEventListener('click', function() {
   numberGuessed.innerText = "-"
 });
 
-//disable and reenable the reset button
-
 var resetButton = document.querySelector('#reset-button');
 
 userInput.addEventListener('keydown', function() {
@@ -167,21 +126,14 @@ resetButton.addEventListener('click', function() {
   resetButton.setAttribute('disabled', 'true');
   clearButton.setAttribute('disabled', 'true');
   numberGuessed.style.display = 'block';
-  hiddenMarvinImg.style.display = 'none'
+  hiddenMarvinImg.style.display = 'none';
   userInput.value = '';
-  numberGuessed.innerText = "-"
-  generateCorrectAnswer()
+  numberGuessed.innerText = "-";
+  generateCorrectAnswer();
 });
-
 
 function unHideMarvin() {
    hiddenMarvinImg.style.display = 'block';
    numberGuessed.style.display = 'none';
-  // hiddenMarvinImg.style.removeProperty('display');
-  // console.log(hiddenMarvinImg.hasAttribute('hidden'));
-  // hiddenMarvinImg.removeAttribute('hidden');
-  // console.log('tried to show Marvin');
-  // numberGuessed.innerText = hiddenMarvinImg;
-  // console.log(hiddenMarvinImg.hasAttribute('hidden'));
 };
 
